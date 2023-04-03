0edf0621ef70a492b43d63ab7d40010947ce625f
ourfalia <ourfalia@tcd.ie>
2023-04-03 07:28:16 +0000

    fix problems

--- reservation/views.py
+++ reservation/views.py
@@ -9,15 +9,29 @@ from reservation.models import Reservation
 # return redirect('confirmation')
 
 
+# def reserve_table(request):
+#     reserve_form = ReserveTableForm()
+
+#     if request.method == 'POST':
+#         reserve_form = ReserveTableForm(request.POST)
+
+#         if reserve_form.is_valid():
+#             reserve_form.save()
+
+#             return redirect(confirmation)
+#     reserve_form = ReserveTableForm()
+#     context = {'form': reserve_form}
+
+#     return render(request, '../templates/book.html', context)
+
+
 def reserve_table(request):
     reserve_form = ReserveTableForm()
 
     if request.method == 'POST':
         reserve_form = ReserveTableForm(request.POST)
-
         if reserve_form.is_valid():
             reserve_form.save()
-
             return render(request, '../templates/confirmation.html')
     reserve_form = ReserveTableForm()
     context = {'form': reserve_form}
@@ -41,10 +55,8 @@ def edit_item(request, item_id):
     item = get_object_or_404(Reservation, id=item_id)
     if request.method == 'POST':
         reserve_form = ReserveTableForm(request.POST, instance=item)
-
         if reserve_form.is_valid():
             reserve_form.save()
-
             return render(request, '../templates/confirmation.html')
     reserve_form = ReserveTableForm(instance=item)
     context = {'form': reserve_form}
@@ -54,7 +66,7 @@ def edit_item(request, item_id):
 def cancel_item(request, item_id):
     item = get_object_or_404(Reservation, id=item_id)
     item.delete()
-    return redirect('cancelation')
+    return render(request, '../templates/cancelation.html')
 
 
 def cancelation(request):
