dd987955d22d9e294bf3676d85cd186dadf3ecae
ourfalia <ourfalia@tcd.ie>
2023-04-03 00:56:18 +0000

    copy and add contact page from previous submission

--- arabicafood/views.py
+++ arabicafood/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+def get_home(request):
+    return render(request, '../templates/home.html')
+
+
+def go_to_menu(request):
+    return render(request, '../templates/menu.html')
+
+
+def go_to_contact(request):
+    return render(request, '../templates/contact.html')
+
+
+def make_reservation(request):
+    return render(request, '../templates/reservation.html')
--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+from .models import Reservation
+from .forms import ReserveTableForm
+from reservation.models import Reservation
 
 # Create your views here.
+
+def reserve_table(request):
+    reserve_form = ReserveTableForm()
+
+    if request.method == 'POST':
+        reserve_form = ReserveTableForm(request.POST)
+
+        if reserve_form.is_valid():
+            reserve_form.save()
+
+            return redirect(confirmation)
+    reserve_form = ReserveTableForm()
+    context = {'form': reserve_form}
+
+    return render(request, 'reservation/reservation.html', context)
